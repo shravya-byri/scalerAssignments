56c930e0e1136d73fe0b2bbdac7f2e3f79e55612
shravya-byri <shravya.byri@gmail.com>
2021-10-27 08:11:49 +0530

    2 power

--- src/Arithmetic/FindingPosition.java
+++ src/Arithmetic/FindingPosition.java
@@ -1,2 +1,16 @@
-package Arithmetic;public class FindingPosition {
+package Arithmetic;
+
+public class FindingPosition {
+    public static void main(String[] args) {
+        int A = 100;
+        System.out.println(solve(A));
+    }
+    public static int solve(int A) {
+        long res = 1;
+       while(res <= A/2) {
+           res = res * 2;
+       }
+
+        return (int)res;
+    }
 }
