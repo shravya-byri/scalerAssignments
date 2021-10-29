4c2c1ecd177222abcd6bc50c0592a9ff46f49440
shravya-byri <shravya.byri@gmail.com>
2021-10-29 20:32:15 +0530

    gcd

---
+++ src/Arithmetic/EnumeratingGCD.java
@@ -0,0 +1,21 @@
+package Arithmetic;
+
+public class EnumeratingGCD {
+    public static void main(String[] args) {
+        String A = "3412";
+        String B = "3413";
+        System.out.println(solve(A, B));
+    }
+
+    public static String solve(String A, String B) {
+        if (A.length() == B.length()) {
+            for (int i = 0; i < A.length(); i++) {
+                if (A.charAt(i) == B.charAt(i)) {
+                    continue;
+                } else return "1";
+            }
+        } else return "1";
+
+        return A;
+    }
+}
