c68de617d9faf8e4ab171b195742585247d42e73
shravya-byri <shravya.byri@gmail.com>
2021-11-07 19:18:18 +0530

    luckynumbers

--- src/Arithmetic/luckyNumbers.java
+++ src/Arithmetic/luckyNumbers.java
@@ -1,2 +1,35 @@
-package Arithmetic;public class luckyNumbers {
+package Arithmetic;
+
+import java.sql.Array;
+import java.util.*;
+
+public class luckyNumbers {
+    public static void main(String[] args) {
+        int A = 12;
+        System.out.println(solve(A));
+    }
+    public static int solve(int A) {
+        int res = 0;
+        ArrayList<Integer> factor = new ArrayList<>();
+       // ArrayList<Integer> num = new ArrayList<>(List.of(0,0));
+        for(int i = 0; i <= A; i++) {
+            factor.add(0);
+        }
+       for(int i = 2; i <= A; i++) {
+           if(factor.get(i) == 0) {
+               for(int j = i; j <= A; j = j+i) {
+                   factor.set(j, factor.get(j)+1);
+               }
+           }
+
+       }
+       int count = 0;
+       for(int i = 0; i <= A; i++) {
+           if(factor.get(i) == 2) {
+               count++;
+           }
+       }
+         System.out.println(factor);
+        return count;
+    }
 }
