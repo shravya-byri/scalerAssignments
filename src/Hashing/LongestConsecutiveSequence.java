73b5dd4285035c5e532b173906ce3ab11dae77d2
shravya-byri <shravya.byri@gmail.com>
2021-12-09 17:45:21 +0530

    scaler

--- src/Hashing/LongestConsecutiveSequence.java
+++ src/Hashing/LongestConsecutiveSequence.java
@@ -1,2 +1,30 @@
-package Hashing;public class LongestConsecutiveSequence {
+package Hashing;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class LongestConsecutiveSequence {
+    public static void main(String[] args) {
+        ArrayList<Integer> A = new ArrayList<>(List.of(100,101,4,200,102,103,104,105,1,3,2));
+        System.out.println(solve(A));
+    }
+    public static int solve(ArrayList<Integer> A) {
+        int count; int ma = 0; int ans = 0;
+        HashSet<Integer> hash = new HashSet<>(A);
+        for(int i = 0; i < A.size(); i++) {
+            if(!(hash.contains(A.get(i)-1))) {
+                count = 1;
+                int x = A.get(i);
+                while(hash.contains(x+1)){
+                    x++;
+                    count++;
+                }
+                ma = Math.max(ma,count);
+            }
+        }
+
+
+        return ma;
+    }
 }
